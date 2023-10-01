1f78e06e1b03b3ef21da92cf8eae7046aca35837
Nikita <daritergha@gmail.com>
2023-10-01 20:31:41 +0300

    Create Program.cs

---
+++ CsharpTelegramBot /Program.cs
@@ -0,0 +1,57 @@
+﻿using CsharpTelegramBot.Config;
+using Telegram.Bot;
+
+namespace CsharpTelegramBot
+{
+    public class Program
+    {
+       static async Task Main(string[] args)
+        {
+            await Task.Run(() =>
+            {
+                EnvLoader.LoadEnv();
+            });
+
+
+            string? botToken = await Task.Run(() =>
+            {
+                return EnvLoader.GetBotToken();
+            });
+
+            if (botToken == ExceptionMessages.TokenDoesNotExistInEnv)
+            {
+                ExceptionMessages.SendSpecialMessage(ConsoleColor.Red, ExceptionMessages.TokenDoesNotExistInEnv);
+                throw new Exception();
+            }
+
+            string? openApiKey = await Task.Run(() =>
+            {
+                return EnvLoader.GetOpenApiKey();
+            });
+
+            if (openApiKey == ExceptionMessages.OpenApiKeyDoesNotExistInEnv)
+            {
+                ExceptionMessages.SendSpecialMessage(ConsoleColor.Red, ExceptionMessages.OpenApiKeyDoesNotExistInEnv);
+                throw new Exception();
+            }
+
+            TelegramBot bot = new TelegramBot(botToken, openApiKey);
+
+            await Task.Run(() =>
+            {
+                bot.Init();
+            });
+
+            await Task.Run(() =>
+            {
+                bot.StartReceiving();
+            });
+
+            var about = await bot.TelegramBotClient.GetMeAsync();
+
+            Console.WriteLine($"Start listening for @{about.Username}");
+
+            await ConsoleHandler.CommandHandler();
+        }
+    }
+}
